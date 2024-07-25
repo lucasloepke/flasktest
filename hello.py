fd447ff5a046705f5f0f6497d790db189808765c
lucasloepke <wavyknight72@gmail.com>
2024-07-25 11:38:55 -0400

    update

--- hello.py
+++ hello.py
@@ -5,8 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def index(name=None):
     return render_template('index.html', person=name)
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
