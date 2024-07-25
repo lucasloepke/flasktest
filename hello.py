7c598c71bbfd2fa2d24cc11460b6b454aee9a20f
lucasloepke <wavyknight72@gmail.com>
2024-07-25 10:53:06 -0400

    update

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! update</p>"
+@app.route('/')
+def index(name=None):
+    return render_template('index.html', person=name)
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
