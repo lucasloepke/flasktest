6bcec8e197b3a695957034a18a25a74214cbba5c
lucasloepke <wavyknight72@gmail.com>
2024-07-29 10:26:37 -0400

    update

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index(name=None):
+def index(name='Guest'):
     return render_template('index.html', person=name)
